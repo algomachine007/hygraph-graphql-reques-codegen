9ee9d4f6199170377fbc49e367a510958713fcaf
algomachine007 <amaben202phantom@gmail.com>
2023-02-11 14:33:42 +0100

    creating the resizer

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,49 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./styles.module.css";
 const Sidebar = () => {
+  const ref = useRef(null);
+
+  const [width, setWidth] = useState(400);
+
+  const [isResizing, setIsResizing] = useState(false);
+  console.log(ref);
+
+  const startResizing = useCallback((mouseDownEvent: any) => {
+    setIsResizing(true);
+  }, []);
+
+  const stopResizing = useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = useCallback(
+    (mouseMoveEvent: any) => {
+      if (isResizing) {
+        setWidth(
+          mouseMoveEvent.clientX - ref?.current.getBoundingClientRect().left,
+        );
+      }
+    },
+    [isResizing],
+  );
+
+  useEffect(() => {
+    window.addEventListener("mousemove", resize);
+    window.addEventListener("mouseup", stopResizing);
+    return () => {
+      window.removeEventListener("mousemove", resize);
+      window.removeEventListener("mouseup", stopResizing);
+    };
+  }, [resize, stopResizing]);
+
   return (
-    <aside className={styles.sidebar}>
-      <div className={styles.control}></div>
+    <aside
+      ref={ref}
+      className={styles.sidebar}
+      style={{ width: `${width}px` }}
+      onMouseDown={(e) => e.preventDefault()}
+    >
+      <div id="resize" className={styles.control} onMouseDown={startResizing} />
       Sidebar
     </aside>
   );
